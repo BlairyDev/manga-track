e1e15fe8f727c14a8d63e7d2cf7283d53b27c7ad
Blair Asistin <162504966+blair-asistin@users.noreply.github.com>
2025-04-11 23:19:18 -0400

    Removed some comments

--- server.js
+++ server.js
@@ -202,27 +202,3 @@ app.get("/series/:id", (req, res) => {
 app.listen(port, () => console.log(`Server running on port ${port}`))
 
 
-//db user
-//db password hse5wPMJHEhJucNt
-
-//mongodb+srv://Cluster47754:<db_password>@cluster47754.xy0zkgy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster47754
-
-// const { MongoClient } = require("mongodb")
-
-// const uri = "mongodb+srv://Cluster47754:n1IEc1k8js6mOs2F@cluster47754.xy0zkgy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster47754"
-
-// const client = new MongoClient(uri);
-// async function run() {
-//   try {
-//     const database = client.db('sample_mflix');
-//     const movies = database.collection('movies');
-//     // Query for a movie that has the title 'Back to the Future'
-//     const query = { title: 'Back to the Future' };
-//     const movie = await movies.findOne(query);
-//     console.log(movie);
-//   } finally {
-//     // Ensures that the client will close when you finish/error
-//     await client.close();
-//   }
-// }
-// run().catch(console.dir);
